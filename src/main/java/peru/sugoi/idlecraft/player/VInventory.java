b6d602805e0bdecdf2bb80e86b457b952b0d62f3
persona0306 <riku7700-touroku@yahoo.co.jp>
2023-05-25 11:19:40 +0900

    Add page setter / getter

--- src/main/java/peru/sugoi/idlecraft/player/VInventory.java
+++ src/main/java/peru/sugoi/idlecraft/player/VInventory.java
@@ -1,15 +1,16 @@
 package peru.sugoi.idlecraft.player;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.UUID;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 public class VInventory {
     private final UUID uid;
@@ -61,11 +62,19 @@ public class VInventory {
         return ImmutableMap.copyOf(commonItemMap);
     }
 
+    public int getPage() {
+    	return inventoryMenu.getPage();
+    }
+
     public OfflinePlayer getPlayer() {
         return Bukkit.getPlayer(uid);
     }
 
     public ImmutableList<ItemStack> getToolList() {
         return ImmutableList.copyOf(toolList);
+    }
+
+    public void setPage(int page) {
+    	inventoryMenu.setPage(page);
     }
 }
